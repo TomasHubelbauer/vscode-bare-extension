eb4c906ca2d48feb21674c4d4924ea6ee35a94d4
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-11-23 23:36:01 +0100

    Use the return of node . to find the temp dir

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ import path from 'path';
 import fs from 'fs';
 
 void async function () {
-  let arg = process.argv[2];
-  if (!arg) {
-    arg = path.join(os.tmpdir(), new Date().valueOf().toString());
-    await fs.promises.mkdir(arg);
-    await fs.promises.writeFile(path.join(arg, 'index.log'), '');
-  }
+  const arg = path.join(os.tmpdir(), new Date().valueOf().toString());
+  await fs.promises.mkdir(arg);
+  await fs.promises.writeFile(path.join(arg, 'index.log'), '');
 
   console.log(arg);
   child_process.exec(`code --extensions-dir ${process.cwd()} --verbose --wait ${arg}`, (error, stdout, stderr) => {
@@ -17,7 +14,7 @@ void async function () {
       throw error;
     }
 
-    console.log(stdout);
-    console.log(stderr);
+    //console.log(stdout);
+    //console.log(stderr);
   });
 }()
